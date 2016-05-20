e857729b0b2633e6b9395d4e082625cdbc74eb30
Carter <carter.appleton@gmail.com>
2016-05-20 14:23:29 -0700

    Adding Console app

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Devices.Bluetooth.Advertisement;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var program = new Program();
+        }
+
+        public Program()
+        {
+            // Create Bluetooth Listener
+            var watcher = new BluetoothLEAdvertisementWatcher();
+            // Only activate the watcher when we're recieving values >= -80
+            watcher.SignalStrengthFilter.InRangeThresholdInDBm = -80;
+
+            // Stop watching if the value drops below -90 (user walked away)
+            watcher.SignalStrengthFilter.OutOfRangeThresholdInDBm = -90;
+
+            watcher.Received += OnAdvertisementReceived;
+
+            // Wait 5 seconds to make sure the device is really out of range
+            watcher.SignalStrengthFilter.OutOfRangeTimeout = TimeSpan.FromMilliseconds(5000);
+            watcher.SignalStrengthFilter.SamplingInterval = TimeSpan.FromMilliseconds(2000);
+        }
+
+        private void OnAdvertisementReceived(BluetoothLEAdvertisementWatcher watcher, BluetoothLEAdvertisementReceivedEventArgs eventArgs)
+        {
+            Console.WriteLine("Saw advertisement!");
+        }
+    }
+}
