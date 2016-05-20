b62670186c40a4eec47b606091369666183790bb
Carter Appleton <capple@microsoft.com>
2016-05-20 14:53:34 -0700

    Successfully compiling and listening for Advertisements

--- Program.cs
+++ Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.System;
+
 using Windows.Devices.Bluetooth.Advertisement;
 
 namespace ConsoleApplication1
@@ -11,29 +13,44 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
+            // Start the program
             var program = new Program();
+
+            // Close on key press
+            Console.ReadLine();
         }
 
         public Program()
         {
             // Create Bluetooth Listener
             var watcher = new BluetoothLEAdvertisementWatcher();
+
+            watcher.ScanningMode = BluetoothLEScanningMode.Active;
+
             // Only activate the watcher when we're recieving values >= -80
             watcher.SignalStrengthFilter.InRangeThresholdInDBm = -80;
 
             // Stop watching if the value drops below -90 (user walked away)
             watcher.SignalStrengthFilter.OutOfRangeThresholdInDBm = -90;
 
+            // Register callback for when we see an advertisements
             watcher.Received += OnAdvertisementReceived;
 
             // Wait 5 seconds to make sure the device is really out of range
             watcher.SignalStrengthFilter.OutOfRangeTimeout = TimeSpan.FromMilliseconds(5000);
             watcher.SignalStrengthFilter.SamplingInterval = TimeSpan.FromMilliseconds(2000);
+
+            // Starting watching for advertisements
+            watcher.Start();
         }
 
         private void OnAdvertisementReceived(BluetoothLEAdvertisementWatcher watcher, BluetoothLEAdvertisementReceivedEventArgs eventArgs)
         {
-            Console.WriteLine("Saw advertisement!");
+            // Tell the user we see an advertisement and print some properties
+            Console.WriteLine(String.Format("Advertisement:"));
+            Console.WriteLine(String.Format("  BT_ADDR: {0}", eventArgs.BluetoothAddress));
+            Console.WriteLine(String.Format("  FR_NAME: {0}", eventArgs.Advertisement.LocalName));
+            Console.WriteLine();
         }
     }
 }
